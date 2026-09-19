3b179a4e4100c1b243bcbcc5bcaf7f18d77699a5
Ewan Croft <me@ewancroft.uk>
2026-09-19 18:45:28 +0100

    docs(cli): expose Jetstream status command

--- src/app/cli/usage.cpp
+++ src/app/cli/usage.cpp
@@ -24,6 +24,7 @@ void print_usage(std::ostream &out) {
         << "  atperson groups\n"
         << "  atperson sync [max-pages]\n"
         << "  atperson jetstream [max-events] [max-ms] [--collections <file>] [--dids <file>]\n"
+        << "  atperson jetstream status [--collections <file>] [--dids <file>]\n"
         << "  atperson daemon [max-cycles]\n"
         << "  atperson rebuild\n"
         << "  atperson compact\n"
